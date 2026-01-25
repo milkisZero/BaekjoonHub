f67a339796cd43763d04cef7cba515ed200a108a
milkisZero <127817037+milkisZero@users.noreply.github.com>
2026-01-25 21:23:26 +0900

    [Bronze V] Title: 성택이의 은밀한 비밀번호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/25372. 성택이의 은밀한 비밀번호/성택이의 은밀한 비밀번호.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define INF INT_MAX
+#define LLINF LLONG_MAX
+#define fastio ios::sync_with_stdio(0), cin.tie(NULL), cout.tie(NULL)
+#define endl '\n'
+#define sp ' '
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using ull = unsigned long long;
+
+int main() {
+    fastio;
+
+    ll t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        cout << (s.length() >= 6 && s.length() <= 9 ? "yes" : "no") << endl;
+    }
+
+    return 0;
+}
