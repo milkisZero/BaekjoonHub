adc6451f38f417e4a42593ec310c1362a5e32574
milkisZero <127817037+milkisZero@users.noreply.github.com>
2026-02-24 16:10:36 +0900

    [Gold I] Title: 팰린드롬 분할, Time: 16 ms, Memory: 8172 KB -BaekjoonHub

--- 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.cc
+++ 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.cc
@@ -10,6 +10,7 @@ using pii = pair<int, int>;
 using pll = pair<ll, ll>;
 using ull = unsigned long long;
 
+bool isPal[2505][2505];
 ll dp[2505];
 
 int main() {
@@ -18,22 +19,26 @@ int main() {
     string s;
     cin >> s;
 
+    for (int i = 0; i < s.size(); i++) isPal[i][i] = 1;
+
+    for (int len = 2; len <= s.size(); len++) {
+        for (int i = 0; i + len - 1 < s.size(); i++) {
+            int j = i + len - 1;
+            if (s[i] == s[j]) {
+                if (len == 2) isPal[i][j] = 1;
+                else isPal[i][j] = isPal[i + 1][j - 1];
+            }
+        }
+    }
+
     fill(&dp[0], &dp[2505], LLINF);
 
     dp[0] = 1;
     for (int i = 1; i < s.size(); i++) {
-        string tmp = "";
-        string tmp2 = "";
-
         for (int j = i; j >= 0; j--) {
-            tmp += s[j];
-            tmp2 = s[j] + tmp2;
-
-            if (s[j] == s[i]) {
-                if (tmp == tmp2) {
-                    if (j == 0) dp[i] = 1;
-                    else dp[i] = min(dp[j - 1] + 1, dp[i]);
-                }
+            if (isPal[j][i]) {
+                if (j == 0) dp[i] = 1;
+                else dp[i] = min(dp[i], dp[j - 1] + 1);
             }
         }
     }
