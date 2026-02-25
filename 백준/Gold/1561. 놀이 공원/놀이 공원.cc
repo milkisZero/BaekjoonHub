fbb4e2dd49cea350886698872be4755168723804
milkisZero <127817037+milkisZero@users.noreply.github.com>
2026-02-25 15:34:24 +0900

    [Gold I] Title: 놀이 공원, Time: 4 ms, Memory: 2220 KB -BaekjoonHub

---
+++ 백준/Gold/1561. 놀이 공원/놀이 공원.cc
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+#define INF INT_MAX
+#define LLINF LLONG_MAX
+#define fastio ios::sync_with_stdio(0), cin.tie(NULL), cout.tie(NULL)
+#define endl '\n'
+#define sp ' '
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using ull = unsigned long long;
+
+int main() {
+    fastio;
+
+    ll n, m;
+    cin >> n >> m;
+
+    vector<ll> v(m), v2(m);
+    ll maxi = 0;
+    for (int i = 0; i < m; i++) {
+        cin >> v[i];
+        maxi = max(maxi, v[i]);
+    }
+
+    if (n <= m) {
+        cout << n;
+        return 0;
+    }
+
+    n -= m;
+
+    ll l = 0, r = maxi * n, mid;
+    ll res = 0, time = 0;
+    while (l <= r) {
+        mid = (l + r) / 2;
+
+        ll cnt = 0;
+        for (int i = 0; i < m; i++) {
+            cnt += (mid / v[i]);
+        }
+
+        if (cnt >= n) r = mid - 1;
+        else {
+            res = cnt;
+            time = mid;
+            l = mid + 1;
+        }
+    }
+
+    n -= res;
+    while (1) {
+        time++;
+        for (int i = 0; i < m; i++) {
+            if (time % v[i] == 0) {
+                n--;
+                if (n == 0) {
+                    cout << i + 1;
+                    return 0;
+                }
+            }
+        }
+    }
+
+    return 0;
+}
