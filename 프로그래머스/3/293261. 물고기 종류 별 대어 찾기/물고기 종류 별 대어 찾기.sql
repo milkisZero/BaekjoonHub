01e978f24b9d27f78245a86be61b5dab1776116c
milkisZero <127817037+milkisZero@users.noreply.github.com>
2026-02-25 18:13:54 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -0,0 +1,10 @@
+select distinct a.ID, b.FISH_NAME, c.maxi as LENGTH
+from FISH_INFO a
+join FISH_NAME_INFO b on a.FISH_TYPE = b.FISH_TYPE
+join (
+    select FISH_TYPE, max(LENGTH) as maxi
+    from FISH_INFO
+    where LENGTH is not null
+    group by FISH_TYPE 
+) c on c.maxi = a.LENGTH and c.FISH_TYPE = a.FISH_TYPE
+order by ID asc
