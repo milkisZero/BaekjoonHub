c0c505ab785832617fe2682efe94a227856f00d6
milkisZero <127817037+milkisZero@users.noreply.github.com>
2026-02-26 14:59:25 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 20 ms, Memory: 2080 KB -BaekjoonHub

---
+++ 백준/Bronze/9093. 단어 뒤집기/README.md
@@ -0,0 +1,28 @@
+# [Bronze I] 단어 뒤집기 - 9093 
+
+[문제 링크](https://www.acmicpc.net/problem/9093) 
+
+### 성능 요약
+
+메모리: 2080 KB, 시간: 20 ms
+
+### 분류
+
+구현, 문자열
+
+### 제출 일자
+
+2026년 2월 26일 14:59:16
+
+### 문제 설명
+
+<p>For each list of words, print a line with each word reversed without changing the order of the words. Each word consists of English letters only.</p>
+
+### 입력 
+
+ <p>The input consists of T test cases. The number of test cases (T) is given in the first line of the input file. Each test case consists of a single line: each line contains a list of words separated by one space.</p>
+
+### 출력 
+
+ <p>For each test case, print the output on one line.</p>
+
---
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define INF INT_MAX
+#define LLINF LLONG_MAX
+#define fastio ios::sync_with_stdio(0), cin.tie(NULL), cout.tie(NULL)
+#define endl '\n'
+#define sp ' '
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using ull = unsigned long long;
+
+int main() {
+    fastio;
+
+    ll n;
+    cin >> n;
+    cin.ignore();
+
+    while (n--) {
+        string s;
+        getline(cin, s);
+        istringstream iss(s);
+
+        string a;
+        while (getline(iss, a, ' ')) {
+            reverse(a.begin(), a.end());
+            cout << a << sp;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
