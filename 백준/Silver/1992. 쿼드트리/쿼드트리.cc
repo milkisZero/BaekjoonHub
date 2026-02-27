fab3df0e56ba604de112595be7ccd4dcd3d50fef
milkisZero <127817037+milkisZero@users.noreply.github.com>
2026-02-27 22:53:04 +0900

    [Silver I] Title: 쿼드트리, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.cc
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.cc
@@ -12,30 +12,24 @@ using ull = unsigned long long;
 
 vector<string> v;
 
-bool check(ll r1, ll r2, ll c1, ll c2) {
-    bool w = 0, b = 0;
-    for (int i = r1; i < r2; i++) {
-        for (int j = c1; j < c2; j++) {
-            if (v[i][j] == '1') w = 1;
-            else b = 1;
+bool check(ll r, ll c, ll n) {
+    for (int i = r; i < r + n; i++) {
+        for (int j = c; j < c + n; j++) {
+            if (v[r][c] != v[i][j]) return 1;
         }
     }
-    if (w + b == 1) {
-        if (w) cout << '1';
-        else cout << '0';
-        return 0;
-    }
-    return 1;
+    cout << v[r][c];
+    return 0;
 }
 
 void recur(ll r1, ll c1, ll n) {
     n /= 2;
 
     cout << "(";
-    if (check(r1, r1 + n, c1, c1 + n)) recur(r1, c1, n);
-    if (check(r1, r1 + n, c1 + n, c1 + n * 2)) recur(r1, c1 + n, n);
-    if (check(r1 + n, r1 + 2 * n, c1, c1 + n)) recur(r1 + n, c1, n);
-    if (check(r1 + n, r1 + 2 * n, c1 + n, c1 + 2 * n)) recur(r1 + n, c1 + n, n);
+    if (check(r1, c1, n)) recur(r1, c1, n);
+    if (check(r1, c1 + n, n)) recur(r1, c1 + n, n);
+    if (check(r1 + n, c1, n)) recur(r1 + n, c1, n);
+    if (check(r1 + n, c1 + n, n)) recur(r1 + n, c1 + n, n);
     cout << ")";
 
     return;
@@ -50,7 +44,7 @@ int main() {
     v.resize(n);
     for (auto &e : v) cin >> e;
 
-    if (check(0, n, 0, n)) recur(0, 0, n);
+    if (check(0, 0, n)) recur(0, 0, n);
 
     return 0;
 }
