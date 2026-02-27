336a6f1cabb21aa9f24951468b310eba5b83b2bc
milkisZero <127817037+milkisZero@users.noreply.github.com>
2026-02-27 22:36:04 +0900

    [Silver I] Title: 쿼드트리, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.cc
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.cc
@@ -12,55 +12,37 @@ using ull = unsigned long long;
 
 vector<string> v;
 
-void recur(ll r1, ll r2, ll c1, ll c2, ll n) {
+bool check(ll r1, ll r2, ll c1, ll c2) {
+    bool w = 0, b = 0;
+    for (int i = r1; i < r2; i++) {
+        for (int j = c1; j < c2; j++) {
+            if (v[i][j] == '1') w = 1;
+            else b = 1;
+        }
+    }
+
+    if (w + b == 1) {
+        if (w) cout << '1';
+        else cout << '0';
+        return 0;
+    }
+
+    return 1;
+}
+
+void recur(ll r1, ll c1, ll n) {
 
     n /= 2;
 
     cout << "(";
 
-    bool w = 0, b = 0;
-    for (int i = r1; i < r1 + n; i++) {
-        for (int j = c1; j < c1 + n; j++) {
-            if (v[i][j] == '1') w = 1;
-            else b = 1;
-        }
-    }
-    if (w && b == 0) cout << '1';
-    else if (w == 0 && b) cout << '0';
-    else recur(r1, r1 + n, c1, c1 + n, n);
+    if (check(r1, r1 + n, c1, c1 + n)) recur(r1, c1, n);
 
-    w = 0, b = 0;
-    for (int i = r1; i < r1 + n; i++) {
-        for (int j = c1 + n; j < c1 + 2 * n; j++) {
-            if (v[i][j] == '1') w = 1;
-            else b = 1;
-        }
-    }
-    if (w && b == 0) cout << '1';
-    else if (w == 0 && b) cout << '0';
-    else recur(r1, r1 + n, c1 + n, c1 + 2 * n, n);
+    if (check(r1, r1 + n, c1 + n, c1 + n * 2)) recur(r1, c1 + n, n);
 
-    w = 0, b = 0;
-    for (int i = r1 + n; i < r1 + 2 * n; i++) {
-        for (int j = c1; j < c1 + n; j++) {
-            if (v[i][j] == '1') w = 1;
-            else b = 1;
-        }
-    }
-    if (w && b == 0) cout << '1';
-    else if (w == 0 && b) cout << '0';
-    else recur(r1 + n, r1 + 2 * n, c1, c1 + n, n);
+    if (check(r1 + n, r1 + 2 * n, c1, c1 + n)) recur(r1 + n, c1, n);
 
-    w = 0, b = 0;
-    for (int i = r1 + n; i < r1 + 2 * n; i++) {
-        for (int j = c1 + n; j < c1 + 2 * n; j++) {
-            if (v[i][j] == '1') w = 1;
-            else b = 1;
-        }
-    }
-    if (w && b == 0) cout << '1';
-    else if (w == 0 && b) cout << '0';
-    else recur(r1 + n, r1 + 2 * n, c1 + n, c1 + 2 * n, n);
+    if (check(r1 + n, r1 + 2 * n, c1 + n, c1 + 2 * n)) recur(r1 + n, c1 + n, n);
 
     cout << ")";
 
@@ -74,20 +56,9 @@ int main() {
     cin >> n;
 
     v.resize(n);
-    for (auto &e : v) {
-        cin >> e;
-    }
+    for (auto &e : v) cin >> e;
 
-    bool w = 0, b = 0;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (v[i][j] == '1') w = 1;
-            else b = 1;
-        }
-    }
-    if (w && b == 0) cout << '1';
-    else if (w == 0 && b) cout << '0';
-    else recur(0, n, 0, n, n);
+    if (check(0, n, 0, n)) recur(0, 0, n);
 
     return 0;
 }
