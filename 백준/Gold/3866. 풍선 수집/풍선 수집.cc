361bbf195808c6561b915c2b9c5524bda86f9281
milkisZero <127817037+milkisZero@users.noreply.github.com>
2026-03-04 20:27:02 +0900

    [Gold III] Title: 풍선 수집, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/3866. 풍선 수집/풍선 수집.cc
+++ 백준/Gold/3866. 풍선 수집/풍선 수집.cc
@@ -10,9 +10,6 @@ using pii = pair<int, int>;
 using pll = pair<ll, ll>;
 using ull = unsigned long long;
 
-// i = 풍선 번호
-// j = 현재 차량의 풍선 개수
-// val = 최소 이동 거리
 ll dp[44][4];
 
 int main() {
@@ -28,32 +25,27 @@ int main() {
 
         fill(&dp[0][0], &dp[43][3], LLINF);
 
-        ll idx = 0;
-        // 1 칸 이동하는데 k+1 시간
-        for (int i = 0; i < n; i++) {
-            idx = i;
+        int i = 0;
+        for (i = 0; i < n; i++) {
             if (i == 0) {
                 if (v[0].first <= v[0].second) {
                     dp[0][1] = v[0].first;
                     continue;
                 }
-                else break;
+                break;
             }
 
             ll time = v[i - 1].second;
             bool k = 0;
-
             for (int j = 0; j <= 3; j++) {
-                bool ini = dp[i - 1][j] == LLINF;
+                if (dp[i - 1][j] == LLINF) continue;
 
-                // 집에 들려서 차량 비우고 담기 - 시간 체크
-                if (!ini && v[i].second >= time + v[i - 1].first * (j + 1) + v[i].first) {
+                if (v[i].second >= time + v[i - 1].first * (j + 1) + v[i].first) {
                     dp[i][1] = min(dp[i][1], dp[i - 1][j] + v[i - 1].first + v[i].first);
                     k = 1;
                 }
 
-                // 집에 안들리고 바로 담기
-                if (j < 3 && !ini && v[i].second >= time + abs(v[i - 1].first - v[i].first) * (j + 1)) {
+                if (j < 3 && v[i].second >= time + abs(v[i - 1].first - v[i].first) * (j + 1)) {
                     dp[i][j + 1] = min(dp[i][j + 1], dp[i - 1][j] + abs(v[i - 1].first - v[i].first));
                     k = 1;
                 }
@@ -62,17 +54,10 @@ int main() {
         }
 
         ll mini = LLINF;
-        for (int j = 0; j <= 3; j++) {
-            mini = min(mini, dp[n - 1][j]);
-        }
+        for (int j = 0; j <= 3; j++) mini = min(mini, dp[n - 1][j]);
 
-        if (mini == LLINF) {
-            cout << "NG " << idx + 1 << endl;
-        }
-        else {
-            ll res = mini + v[n - 1].first;
-            cout << "OK " << res << endl;
-        }
+        if (mini == LLINF) cout << "NG " << i + 1 << endl;
+        else cout << "OK " << mini + v[n - 1].first << endl;
     }
 
     return 0;
