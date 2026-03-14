77cd8ad6beeb8c14dae6ed18e29ad378a374ae44
milkisZero <127817037+milkisZero@users.noreply.github.com>
2026-03-14 19:15:04 +0900

    [Silver V] Title: 수들의 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1789. 수들의 합/수들의 합.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define INF INT_MAX
+#define LLINF LLONG_MAX
+#define fastio ios::sync_with_stdio(0), cin.tie(NULL), cout.tie(NULL)
+#define endl '\n'
+#define sp ' '
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using ull = unsigned long long;
+
+int main() {
+    fastio;
+
+    ll n;
+    cin >> n;
+
+    ll k = 1;
+    while (1) {
+        if (n - k * (k + 1) / 2 < k+1) break;
+        k++;
+    }
+
+    cout << k;
+
+    return 0;
+}
