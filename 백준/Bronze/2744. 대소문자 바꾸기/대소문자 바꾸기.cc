91b12f519d660cd8060405f532399547105256d1
milkisZero <127817037+milkisZero@users.noreply.github.com>
2026-04-05 22:01:29 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define INF INT_MAX
+#define LLINF LLONG_MAX
+#define fastio ios::sync_with_stdio(0), cin.tie(NULL), cout.tie(NULL)
+#define endl '\n'
+#define sp ' '
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using ull = unsigned long long;
+
+int main() {
+    fastio;
+
+    string s;
+    cin >> s;
+
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] >= 'a') cout << (char)(s[i] - ('a' - 'A'));
+        else cout << (char)(s[i] + ('a' - 'A'));
+    }
+
+    return 0;
+}
